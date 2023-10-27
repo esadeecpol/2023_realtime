b229c99b710a859e7afb4607eb26e2ecb16abe22
Javi Martinez <javier.martinez.snt@gmail.com>
2023-10-27 13:48:27 +0200

    Update

---
+++ static/js/app.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Comprobar si el usuario ya ha visitado el sitio
+    if (!localStorage.getItem('visitedBefore')) {
+      // Mostrar el cuadro de diálogo
+      var modal = document.getElementById('welcomeModal');
+      modal.style.display = 'block';
+      
+      // Marcar en localStorage que el usuario ya ha visitado el sitio
+      localStorage.setItem('visitedBefore', 'true');
+    }
+  
+    // Cerrar el cuadro de diálogo
+    var closeBtn = document.querySelector('.close-btn');
+    closeBtn.addEventListener('click', function() {
+      var modal = document.getElementById('welcomeModal');
+      modal.style.display = 'none';
+    });
+  });
+  
