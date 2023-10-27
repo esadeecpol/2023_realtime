61ce8941cfc8b742e77cb41c67f134a3d63f369e
Javi Martinez <javier.martinez.snt@gmail.com>
2023-10-27 13:41:11 +0200

    Update

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def principal():
+def main():
     return render_template('trimestral.html')
 
 
-@app.route('/Anual')
-def le():
+@app.route('/Anual.html')
+def anual():
     return render_template('anual.html')
 
-
-@app.route('/Metodologia')
-def contacto():
+@app.route('/Metodologia.html')
+def metodologia():
     return render_template('metodologia.html')
 
 
+freezer = Freezer(app)
 if __name__ == '__main__':
-    app.run(debug=True)
+    freezer.freeze()
