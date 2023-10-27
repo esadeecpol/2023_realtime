feb2762fe9d873b38fee8e7a8c98a6e77862b412
Javi Martinez <javier.martinez.snt@gmail.com>
2023-10-27 13:54:04 +0200

    Update

--- build/static/js/app.js
+++ build/static/js/app.js
@@ -1,13 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Comprobar si el usuario ya ha visitado el sitio
-    if (!localStorage.getItem('visitedBefore')) {
-      // Mostrar el cuadro de diálogo
-      var modal = document.getElementById('welcomeModal');
-      modal.style.display = 'block';
-      
-      // Marcar en localStorage que el usuario ya ha visitado el sitio
-      localStorage.setItem('visitedBefore', 'true');
-    }
+    // Mostrar el cuadro de diálogo
+    var modal = document.getElementById('welcomeModal');
+    modal.style.display = 'block';
   
     // Cerrar el cuadro de diálogo
     var closeBtn = document.querySelector('.close-btn');
